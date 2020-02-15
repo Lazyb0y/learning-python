6ede95c341e7990365d6c43221a7a716ea5de03d
Foysal Ahamed <thisisfoysal@gmail.com>
2020-02-15 15:52:01 +0600

    Add numeric_data.py

---
+++ scripts/numeric_data.py
@@ -0,0 +1,34 @@
+num = 3
+print(type(num))
+
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+print(abs(-3))
+print(round(3.75))
+print(round(3.75, 1))
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 <= num_2)
+
+num_1 = '100'
+num_2 = '200'
+
+print(int(num_1) + int(num_2))
